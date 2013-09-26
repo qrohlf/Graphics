141fb1d56a10de4d7acb13d8785b4281c2ba5ce9
Quinn Rohlf <qr@qrohlf.com>
2013-09-26 10:53:45 -0700

    add unmodified D2d stuff...

--- scratch.c
+++ scratch.c
@@ -1,16 +1,8 @@
-#include <qmlib.h>
+#include <qdmlib.h>
 #include <FPT.h>
 
 int main(int argc, char *argv[]) {
-    double foo[] = {1, 3, 2, 5, 4};
-    printf("Before\n");
-    for(int i=0; i<5; i++) {
-        printf("%2.0f ", foo[i]);
-    }
-    sort_asc(foo, 5);
-    printf("\nAfter\n");
-    for(int i=0; i<5; i++) {
-        printf("%2.0f ", foo[i]);
-    }
-    printf("\n");
+    double a[] = {0, 1};
+    double b[] = {1, 0};
+    printf("Dot %f\n", dot(a, b));
 }
