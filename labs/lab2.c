7728067965e1fb76ccf35a2663b083f96eb9698c
Quinn Rohlf <qr@qrohlf.com>
2013-10-03 12:33:10 -0700

    lab2 mostly working. Just needs centering

--- labs/lab2.c
+++ labs/lab2.c
@@ -1,5 +1,6 @@
 #include <FPT.h>
 #include <qdmlib.h>
+#include <D2d_matrix.h>
 #include <math.h>
 #define true 1
 #define false 0;
@@ -7,8 +8,7 @@
 
 struct shape {
 	int num_vertices;
-	double xs[1000];
-	double yx[1000];
+	int vertices[1000];
 	double R;
 	double G;
 	double B;
@@ -16,74 +16,95 @@ struct shape {
 
 struct drawObject {
 	int num_shapes;
-	struct shape shapes[1000];
+	int num_points;
+	double xs[1000];
+	double ys[1000];
+	struct shape shapes[100];
 };
 
-void draw_shape_from_file(char*, double*, double*, int);
 
-int main(int argc, char *argv[]) {
-	double x[100];
-	double y[100];
-	int n;
-	G_init_graphics(600, 600);
-	G_rgb(1, 0, 0);
-	n = read_points_from_file(fopen(argv[1], "r"), x, y);
-	//rotate_polygon(x, y, n, 0.785);
-	draw_shape_from_file(argv[1], x, y, n);
-	G_wait_key();
-}
-
-void draw_shape_from_file(char* file, double* x, double* y, int vertices) {
-	FILE *f;
-	FILE *colors;
-	double x1[100];
-	double y1[100];
-	int connections;
-	f = fopen(file, "r");
-	colors = fopen(file, "r");
-	if (f==NULL) {
-		printf("can't open file '%s'\n", file);
-		exit(1);
-	}
-	for (int i=0; i<=vertices; i++) {
-		fscanf(f, "%*[^\n]\n", NULL);
-	}
-	fscanf(f, "%d\n", &connections);
-	printf("%d shapes in file\n", connections);
-	for (int i=0; i<=vertices+connections+1; i++) {
-		fscanf(colors, "%*[^\n]\n", NULL);
-	}
-	//char buffer[100];
-	//fgets(buffer, sizeof buffer, colors);
-	//printf("current line that colors is pointing to: %s\n", buffer);
-
-	int pointnum;
-	for (int i=0; i<connections; i++) {
-		fscanf(f, "%d ", &vertices);
-		printf("shape %d has %d vertices:\n", i, vertices);
-		for (int j=0; j<vertices; j++) {
-			fscanf(f, "%d", &pointnum);
-			printf("\t vertex %d: %3f, %3f\n", j, x[pointnum], y[pointnum]);
-			x1[j]=x[pointnum];
-			y1[j]=y[pointnum];
-		}
-		//char buffer[100];
-		//fgets(buffer, sizeof buffer, colors);
-		//printf("current line that colors is pointing to: %s\n", buffer);
-		double r, g, b;
-		fscanf(colors, "%lf %lf %lf", &r, &g, &b);
-		printf("color: %3f, %3f, %3f\n", r, g, b);
-		G_rgb(r, g, b);
-		G_fill_polygon(x1, y1, vertices);
-	}
-}
 
 /*
  * constructs an object from a list of points, connections, and colors
  */
-void make_drawObject(FILE* f) {
-	struct drawObject obj;
-	int num_points;
-	fscanf("%d", num_points);
+void make_drawObject(FILE* f, struct drawObject *obj) {
+	// Get the points
+	fscanf(f, "%d", &obj->num_points);
+	printf("num_points is %d\n", obj->num_points);
+	for (int i=0; i<obj->num_points; i++) {
+		fscanf(f, "%lf %lf", &obj->xs[i], &obj->ys[i]);
+	}
+
+
+	// Get the shapes
+	fscanf(f, "%d", &obj->num_shapes);
+	for (int i=0; i<obj->num_shapes; i++) {
+		fscanf(f, "%d", &obj->shapes[i].num_vertices);
+		for (int j=0; j<obj->shapes[i].num_vertices; j++) {
+			fscanf(f, "%d", &obj->shapes[i].vertices[j]);
+		}
+	}
+
+	// Get the colors
+	for (int i=0; i<obj->num_shapes; i++) {
+		fscanf(f, "%lf %lf %lf", &obj->shapes[i].R, &obj->shapes[i].G, &obj->shapes[i].B);
+	}
 }
+
+void draw_drawObject(struct drawObject obj) {
+	struct shape *s;
+	double x[1000];
+	double y[1000];
+	int point_index;
+	printf("%d shapes in object to draw\n", obj.num_shapes);
+	for (int i=0; i<obj.num_shapes; i++) {
+		s = &obj.shapes[i];
+		printf("%d vertices in shape %d\n", s->num_vertices, i);
+		for (int j=0; j<s->num_vertices; j++) {
+			point_index = s->vertices[j];
+			x[j] = obj.xs[point_index];
+			y[j] = obj.ys[point_index];
+		}
+		G_rgb(s->R, s->G, s->B);
+		printf("Color is %f %f %f\n", s->R, s->G, s->B);
+		G_fill_polygon(x, y, s->num_vertices);
+	}
+}
+
+int main(int argc, char *argv[]) {
+	struct drawObject objects[10];
+	FILE *f;
+	int num_objects = argc-1;
+	for (int i=0; i<num_objects; i++) {
+		f = fopen(argv[i+1], "r");
+		if (f == NULL) {
+			printf("Invalid file %s\n", argv[i+1]);
+			exit(1);
+		}
+		make_drawObject(f, &objects[i]);
+	}
+	printf("%d objects loaded. Press any digit to draw.\n", num_objects);
 	
+	G_init_graphics(600, 600);
+	int c;
+	int i;
+	double rot[3][3];
+	double rot_inv[3][3];
+	D2d_make_identity(rot);
+	D2d_rotate(rot, rot_inv, M_PI/180.0);
+	//D2d_make_identity(rot);
+	while(true) {
+		G_rgb(1, 1, 1);
+		c = G_wait_key();
+		G_clear();
+		i = c - 48; //Convert ASCII character codes to digits
+		printf("i is %d\n", i);
+		if (i < num_objects+1 && i >= 1) {
+			D2d_mat_mult_points (objects[i-1].xs, objects[i-1].ys, rot, objects[i-1].xs, objects[i-1].ys, objects[i-1].num_points);
+			draw_drawObject(objects[i-1]);
+		} else {
+			printf("Invalid selection. Please press a key from 1-%d\n", num_objects);
+		}
+	}
+	
+}
