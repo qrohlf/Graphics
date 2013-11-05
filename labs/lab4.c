3d6b0fca9d4a5a928a94f09f41575ce751c40077
Quinn Rohlf <qr@qrohlf.com>
2013-11-05 11:02:38 -0800

    Update lab4.c for multiple objects

--- labs/lab4.c
+++ labs/lab4.c
@@ -18,18 +18,20 @@ double x_ccw[4][4],
     y_minus[4][4],
     y_plus[4][4],
     z_minus[4][4],
-    z_plus[4][4];
+    z_plus[4][4],
+    all_scale_up[4][4],
+    all_scale_down[4][4];
 
 void setup_matrices() {
     D3d_make_identity(x_ccw);
     D3d_make_identity(x_cw);
-    D3d_rotate_x(x_ccw, x_cw, M_PI/90);
+    D3d_rotate_x(x_ccw, x_cw, M_PI/60);
     D3d_make_identity(y_ccw);
     D3d_make_identity(y_cw);
-    D3d_rotate_y(y_ccw, y_cw, M_PI/90);
+    D3d_rotate_y(y_ccw, y_cw, M_PI/60);
     D3d_make_identity(z_ccw);
     D3d_make_identity(z_cw);
-    D3d_rotate_z(z_ccw, z_cw, M_PI/90);
+    D3d_rotate_z(z_ccw, z_cw, M_PI/60);
     D3d_make_identity(x_plus);
     D3d_make_identity(x_minus);
     D3d_translate(x_plus, x_minus, .2, 0, 0);
@@ -39,17 +41,24 @@ void setup_matrices() {
     D3d_make_identity(z_plus);
     D3d_make_identity(z_minus);
     D3d_translate(z_plus, z_minus, 0, 0, .2);
+    D3d_make_identity(all_scale_up);
+    D3d_make_identity(all_scale_down);
+    D3d_scale(all_scale_up, all_scale_down, 1.005, 1.005, 1.005);
 }
 
 int main(int argc, char const *argv[]) {
-    object3d obj;
-    FILE* f = fopen(argv[1], "r");
-    read_object3d_from_file(f, &obj);
+    object3d objs[9];
+    for (int i=1; i<argc; i++) {
+        FILE* f = fopen(argv[i], "r");
+        read_object3d_from_file(f, &objs[i-1]);
+    }
+    object3d* obj = &objs[0];
     setup_matrices();
     
-    char c;
-    double fov = 150;//M_PI/2.0;
-    double viewdistance = max(obj.zs, obj.n)+1;
+    char c, i;
+    double fov = M_PI/8.0;
+    double viewdistance = max(obj->zs, obj->n)+10;
+    printf("initial viewdistance is %f\n", viewdistance);
     // double ztrans[4][4], useless[4][4];
     // D3d_make_identity(ztrans);
     // D3d_translate(ztrans, useless, 0, 0, -3);
@@ -57,56 +66,67 @@ int main(int argc, char const *argv[]) {
     // transform_object3d(&obj, ztrans);
     // print_object3d(&obj);
     G_init_graphics(600, 600);
-    draw_object3d(&obj, fov, viewdistance);
 
     while(true) {
-        c = G_wait_key();
+        i = c - 48; //Convert ASCII character codes to digits
+        if (i > 0 && i < argc) {
+            printf("Switching control to shape %d\n", i);
+            obj = &objs[i-1];
+        }
         switch (c) {
             case 84:
                 //Down arrow key
-                transform_object3d(&obj, x_cw);
+                transform_object3d(obj, x_cw);
                 break;
             case 82:
                 //Up arrow key
-                transform_object3d(&obj, x_ccw);
+                transform_object3d(obj, x_ccw);
                 break;
             case 81:
                 //Left arrow key
-                transform_object3d(&obj, y_ccw);
+                transform_object3d(obj, y_ccw);
                 break;
             case 83:
                 //Right arrow key
-                transform_object3d(&obj, y_cw);
+                transform_object3d(obj, y_cw);
                 break;
             case 'w':
-                transform_object3d(&obj, y_plus);
+                transform_object3d(obj, y_plus);
                 break;
             case 'a':
-                transform_object3d(&obj, x_minus);
+                transform_object3d(obj, x_minus);
                 break;
             case 's':
-                transform_object3d(&obj, y_minus);
+                transform_object3d(obj, y_minus);
                 break;
             case 'd':
-                transform_object3d(&obj, x_plus);
+                transform_object3d(obj, x_plus);
                 break;
             case 'q':
-                transform_object3d(&obj, z_minus);
+                transform_object3d(obj, z_minus);
                 break;
             case 'e':
-                transform_object3d(&obj, z_plus);
+                transform_object3d(obj, z_plus);
+                break;
+            case ']':
+                transform_object3d(obj, all_scale_up);
+                break;
+            case '[':
+                transform_object3d(obj, all_scale_down);
                 break;
             case '=':
-                viewdistance -= .2;
-                break;
-            case '-':
                 viewdistance += .2;
                 break;
+            case '-':
+                viewdistance -= .2;
+                break;
             case '+':
-                fov += 10;
+                //Smaller FOV zooms in
+                fov -= M_PI/36;
                 break;
             case '_':
-                fov -= 10;
+                //Larger FOV zooms out
+                fov += M_PI/36;
                 break;
             case 32:
                 //Spacebar
@@ -114,7 +134,16 @@ int main(int argc, char const *argv[]) {
         }
         G_rgb(1, 1, 1);
         G_clear();
-        draw_object3d(&obj, fov, viewdistance);
+        for (int j=0; j<argc; j++) {
+            draw_object3d(&objs[j], fov, viewdistance);
+        }
+        char fovstring[128];
+        sprintf(fovstring, "Fov: %.3f rad", fov);
+        char camerastring[128];
+        sprintf(camerastring, "Camera: x:0 y:0 z:%f", viewdistance);
+        G_draw_string(fovstring, 0, 580);
+        G_draw_string(camerastring, 0, 560);
+        c = G_wait_key();
     }
     return 0;
 }
